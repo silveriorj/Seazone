821cf8e0530cb6bdafee1466f7dd11dcff194705
Raul Silvério <rjosesilverio@gmail.com>
2019-11-10 22:19:25 -0300

    Parsing all data

--- Seazone/spiders/airbnb.py
+++ Seazone/spiders/airbnb.py
@@ -3,10 +3,16 @@ import lxml.html as parser
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from threading import Thread,Condition
+import time
+import random
 
-APTOs_LIST = '//*[@class="_14csrlku"]'
-BASE_URL = 'https://www.airbnb.com.br/s/'
+
+APTOs_LIST = '//div[@class="_gig1e7"]'
+BASE_URL = 'https://www.airbnb.com.br'
 
 def new_aptos(driver, min_len):
     return len(driver.find_elements_by_xpath(APTOs_LIST)) > min_len
@@ -29,20 +35,21 @@ class AirbnbCrawl(object):
         self.driver.close()
 
     def crawl_list(self, search_list):
-        print('[INFO] Getting the Apartment list...')
         for term in search_list:
             url = BASE_URL + term
-            name = term.split('~')[0]
-            name = name.replace("--",'_')
-            name = name.replace("-",'')
-            print("[INFO] Extracting the apartments of "+name+"...")
+            name = term.split('--')[0]
+            name = name.replace("/s/",'')
+            print('\033[34m'+"[INFO] Extracting the apartments of "+name+"..."+'\033[0;0m')
             self.crawl_url(url)
             image_name = name + ".png"
             self.screenshot(image_name)
 
     def crawl_url(self, url):
+        print('[INFO] URL:',url)
         self.driver.get(url)
-        self.get_aptos(150)
+        print('[INFO] Getting aptos...')
+        self.get_aptos(160)
+        print('[INFO] Parsing... Wait a fell minutes...')
         return self.parse_aptos()
 
     def get_aptos(self, num_of_aptos):
@@ -50,7 +57,7 @@ class AirbnbCrawl(object):
         while len(aptos) < num_of_aptos:
             try:
                 self.go_bottom()
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 0.2).until(
                     lambda driver: new_aptos(driver, len(aptos)))
             except TimeoutException:
                 # simple exception handling, just move on in case of Timeout
@@ -76,35 +83,59 @@ class AirbnbCrawl(object):
         aptos = html.xpath(APTOs_LIST)
         for apt in aptos:
             #self.info_apart(apt)
-            ad = apt.xpath(
-                ".//*[(@class='_1dss1omb')]/text()")
+            url = apt.xpath('.//a[(@class="_15tommw")]/@href')
             price = apt.xpath(
-                ".//*[(@class='_1jlnvra2')]/descendant-or-self::*"
-                "/text()")
+                ".//*[(@class='_1jlnvra2')]/text()")
+            stars = apt.xpath('.//*[(@class="_tghtxy2")]/text()')
+            print(url)   # For Simple Debbuging
+            print(price) # For Simple Debbuging
             try:
-                stars = apt.xpath(
-                    ".//*[(@class='_tghtxy2')]"
-                    "/descendant-or-self::*/text()")
-            except:
-                stars = "NOVO"
-            try:
-                price = price[2]
-                price = price.replace('R$', '')
-                gains = str(float(price)*120) # Multiplica o preço/noite pela quantidade de dias em 6 meses
+                url = url[0]
             except IndexError:
-                price = 0
-                gains = 0
+                url = 'NaN'
+            try:
+                stars = stars[0]
+            except IndexError:
+                stars = 'NOVO'
+            try:
+                price = price[1]
+                price = price.replace('.', '')
+                price = price.replace('R$', '')
+            except IndexError:
+                price = apt.xpath('.//span[(@class="_1jlnvra2")]/text()')
+            gains = self.info_apart(url)
+            print(gains)
             self.apartment_list.append({
-                "Anúncio": ad,
                 "Preço": price,
                 "Avaliação": stars,
-                "Ganhos em 6 meses": gains
+                "Ganhos 6 Meses": gains,
+                "URL": url
             })
         return self.apartment_list
 
-    def info_apart(self,apart):
-        link =  apart.xpath(".//a[(@class='_1ol0z3h')]")
-        print(link)
+    def info_apart(self,url):
+        url = BASE_URL+url
+        aluguel = 0
+        i, x = 0,0
+        self.driver.get(url)
+        for i in range(3):
+            html = parser.fromstring(self.driver.page_source)
+            tabela = html.xpath('.//div[(@class="_1lds9wb")]')
+            for alugado in tabela:
+                aluguel += len(alugado.xpath('.//td[(@class="_z39f86g")]'))
+            for x in range (2):
+                try:
+                    WebDriverWait(self.driver, 0.025).until(
+                            lambda driver: self.wait_and_press())
+                except ElementClickInterceptedException:
+                    WebDriverWait(self.driver, 0.025).until(
+                            lambda driver: self.wait_and_press())
+                except TimeoutException:
+                    break
+        return aluguel
+
+    def wait_and_press(self):
+        self.driver.find_element_by_xpath('.//*[(@class="_1h5uiygl")]').send_keys('\n')
 
     def save_items(self):
         print('[INFO] Saving the apartments...')
--- Seazone/spiders/vivareal.py
+++ Seazone/spiders/vivareal.py
@@ -4,14 +4,12 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
+
 
 APTO_XPATH = '//div[@data-type="property"]'
-BASE_URL = 'https://www.vivareal.com.br/aluguel/santa-catarina/florianopolis/bairros/'
+BASE_URL = 'https://www.vivareal.com.br/venda/santa-catarina/florianopolis/bairros/'
 LINK_URL = '//a[@class="property-card__main-link js-carousel-link"]'
-
-def new_aptos(driver, min_len):
-    return len(driver.find_elements_by_xpath(APTO_XPATH)) > min_len
-
 
 class VivaRealCrawler(object):
 
@@ -30,84 +28,88 @@ class VivaRealCrawler(object):
         self.driver.close()
 
     def crawl_list(self, search_list):
-        print('[INFO] Getting the Apartment list...')
         for term in search_list:
             url = BASE_URL + term
+            name = term.split('/')[0]
+            print('\033[34m'+"[INFO] Extracting the apartments of "+name+"..."+'\033[0;0m')
             self.crawl_url(url)
-            image_name = (term.split('/')[0]) + ".png"
-            self.screenshot(image_name)
+            image_name = (name) + ".png"
+            self.screenshot(image_name, url)
 
     def crawl_url(self, url):
+        print('[INFO] URL:',url)
         self.driver.get(url)
-        self.get_aptos(50)
-        return self.parse_aptos()
+        print('[INFO] Getting aptos...')
+        self.get_aptos(160)
+        print('[INFO] GOING TOP...')
+        self.go_top(url)
+        print('[INFO] Parsing...')
+        return self.parse_aptos(200)
 
     def get_aptos(self, num_of_aptos):
-        print('[INFO] Scrolling down the page...')
         aptos = self.driver.find_elements_by_xpath(APTO_XPATH)
-        while len(aptos) < num_of_aptos:
-            try:
-                WebDriverWait(self.driver, 10).until(
-                    lambda driver: new_aptos(driver, len(aptos)))
-            except TimeoutException:
-                # simple exception handling, just move on in case of Timeout
-                aptos = self.driver.find_elements_by_xpath(APTO_XPATH)
-                break
-            aptos = self.driver.find_elements_by_xpath(APTO_XPATH)
+        while True:
+            while len(aptos) < num_of_aptos:
+                self.next_page()
+                aptos.append(self.driver.find_elements_by_xpath(APTO_XPATH))
+            break
         return aptos
-
-    def go_top(self):
+    
+    def next_page(self):
+        javaScript = 'document.querySelector("[title="Próxima página"]"));'
+        try:
+            self.driver.execute_script(javaScript).click()
+            return True
+        except:
+            return False
+              
+    def go_top(self, url):
+        self.driver.get(url)
         self.driver.execute_script(
             "window.scrollTo(0, 0);")
 
-    def screenshot(self, image_name):
-        self.go_top()
+    def screenshot(self, image_name, url):
+        self.go_top(url)
         self.driver.save_screenshot('./Images/VivaReal_'+image_name)
 
-    def parse_aptos(self):
-        html = parser.fromstring(self.driver.page_source)
-        aptos = html.xpath(APTO_XPATH)
-        print('[INFO] Extracting the apartments...')
-        for apt in aptos:
-            per = apt.xpath('.//*[(@class="property-card__price-period")]/text()')
-            url = apt.xpath('.//*[(@href)]/@href')
-            price = apt.xpath('.//*[(@class="property-card__price js-property-card-prices js-property-card__price-small")]/text()')
-            condo = apt.xpath('.//*[(@class="js-condo-price")]/text()')
-            try:
-                url = url[0]
-                url = url.replace(' ','')
-            except:
-                url = ''
-            try:
-                per = per[0]
-                per = per.replace(' ','')
-                per = per.replace('"','')
-                per = per.replace('/','')
-                per = per.replace('\n','')
-            except:
-                per = ''
-            try:
-                price = price[0]
-                price = price.replace(' ','')
-                price = price.replace('"','')
-                price = price.replace('\n','')
-                price = price.replace('R$', '')
-            except:
-                price = 0
-            try:
-                condo = condo[0]
-                condo = condo.replace(' ','')
-                condo = condo.replace('"','')
-                condo = condo.replace('\n','')
-                condo = condo.replace('R$', '')
-            except:
-                condo = 0            
-            self.apartment_list.append({
-                "Preço": price,
-                "Condomínio": condo,
-                "Período": per,
-                "URL": url
-            })
+    def parse_aptos(self, num):
+        ap = len(self.driver.find_elements_by_xpath(APTO_XPATH))
+        while True:
+            while ap < num:
+                html = parser.fromstring(self.driver.page_source)
+                aptos = html.xpath(APTO_XPATH)
+                ap += len(aptos)
+                for apt in aptos:
+                    url = apt.xpath('.//*[(@href)]/@href')
+                    price = apt.xpath('.//*[(@class="property-card__price js-property-card-prices js-property-card__price-small")]/text()')
+                    condo = apt.xpath('.//*[(@class="js-condo-price")]/text()')
+                    try:
+                        url = url[0]
+                    except:
+                        url = 'NaN'
+                    try:
+                        price = price[0]
+                        price = price.replace(' ','')
+                        price = price.replace('"','')
+                        price = price.replace('\n','')
+                        price = price.replace('R$', '')
+                    except:
+                        price = str(0)
+                    try:
+                        condo = condo[0]
+                        condo = condo.replace(' ','')
+                        condo = condo.replace('"','')
+                        condo = condo.replace('\n','')
+                        condo = condo.replace('R$', '')
+                    except:
+                        condo = str(0)            
+                    self.apartment_list.append({
+                        "Preço": price,
+                        "Condomínio": condo,
+                        "URL": url
+                    })
+                self.next_page()
+            break
         return self.apartment_list
 
     def save_items(self):
